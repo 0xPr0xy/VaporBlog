2f1dd243f0f763f59d86c00329bbcfa259830934
Peter IJlst <0xPr0xy@gmail.com>
2017-12-20 15:37:48 +0100

    fix issue with dependencies not resolving

--- Package.swift
+++ Package.swift
@@ -11,33 +11,33 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.2.0")),
         .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
-		.package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.0.0")),
-		.package(url: "https://github.com/vapor/mysql-provider.git", .upToNextMajor(from: "2.0.0")),
-		.package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.0.0")),
-		.package(url: "https://github.com/nodes-vapor/slugify", .upToNextMajor(from: "1.0.0")),
-		.package(url: "https://github.com/vapor-community/markdown-provider.git", .upToNextMajor(from: "1.1.0")),
-		.package(url: "https://github.com/nodes-vapor/paginator.git", .upToNextMajor(from: "0.0.0"))
+	.package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.0.0")),
+	.package(url: "https://github.com/vapor/mysql-provider.git", .upToNextMajor(from: "2.0.0")),
+	.package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.0.0")),
+	.package(url: "https://github.com/nodes-vapor/slugify", .upToNextMajor(from: "1.0.0")),
+	.package(url: "https://github.com/vapor-community/markdown-provider.git", .upToNextMajor(from: "1.1.0")),
+	.package(url: "https://github.com/nodes-vapor/paginator.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         .target(
-			name: "App",
-			dependencies: [
-				"Vapor",
-				"LeafProvider",
-				"FluentProvider",
-				"MySQLProvider",
-				"AuthProvider",
-				"MarkdownProvider",
-				"Slugify",
-				"Paginator"
-			],
-			exclude: [
-				"Config",
-				"Database",
-				"Public",
-				"Resources"
-			]
-		),
+		name: "App",
+		dependencies: [
+			"Vapor",
+			"LeafProvider",
+			"FluentProvider",
+			"MySQLProvider",
+			"AuthProvider",
+			"MarkdownProvider",
+			"Slugify",
+			"Paginator"
+		],
+		exclude: [
+			"Config",
+			"Database",
+			"Public",
+			"Resources"
+		]
+	),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
