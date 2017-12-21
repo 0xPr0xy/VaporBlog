a9983418f0794eeaaba2bb02690571646f9c678c
Peter IJlst <0xPr0xy@gmail.com>
2017-12-21 14:06:10 +0100

    redirect middleware for admin to login when not authenticated

--- Sources/App/Routing/Routes.swift
+++ Sources/App/Routing/Routes.swift
@@ -10,6 +10,7 @@ final class Routes: RouteCollection {
 	let passwordMiddleware: PasswordAuthenticationMiddleware<User>
 	let persistMiddleware: PersistMiddleware<User>
 	let sessionsMiddleware: SessionsMiddleware
+	let redirectMiddleware: RedirectMiddleware
 	
 	init(_ view: ViewRenderer, _ config: Config) {
         self.view = view
@@ -17,27 +18,29 @@ final class Routes: RouteCollection {
 		self.passwordMiddleware = PasswordAuthenticationMiddleware(User.self)
 		self.persistMiddleware = PersistMiddleware(User.self)
 		self.sessionsMiddleware = SessionsMiddleware(MemorySessions())
+		self.redirectMiddleware = RedirectMiddleware.login()
     }
 	
 	func build(_ builder: RouteBuilder) throws {
 
 		let adminBuilder = builder.grouped([
+			self.redirectMiddleware,
 			self.sessionsMiddleware,
 			self.persistMiddleware,
 			self.passwordMiddleware
 		])
 		
 		_ = AdminController(view, config, routeBuilder: adminBuilder)
-		
+		_ = UploadController(view, config, routeBuilder: adminBuilder)
+
 		let loginBuilder = builder.grouped([
 			self.sessionsMiddleware,
 			self.persistMiddleware
 		])
+		_ = LoginController(view, config, routeBuilder: loginBuilder)
+		_ = SearchController(view, config, routeBuilder: loginBuilder)
 		
 		_ = try PageController(view, config, adminRouteBuilder: adminBuilder, publicRouteBuilder: loginBuilder)
 		_ = try ArticleController(view, config, adminRouteBuilder: adminBuilder, publicRouteBuilder: loginBuilder)
-		_ = UploadController(view, config, routeBuilder: adminBuilder)
-		_ = LoginController(view, config, routeBuilder: loginBuilder)
-		_ = SearchController(view, config, routeBuilder: loginBuilder)
 	}
 }
